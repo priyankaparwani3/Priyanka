89c8e2a0ff54b28359f21eda45956511f6b4d8cb
priyankaparwani3 <me.priyankaparwani@gmail.com>
2020-03-16 17:51:12 +0530

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Project2
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i = 0;
+            Console.WriteLine("Minimum Value {0}", int.MinValue);
+        }
+    }
+}
