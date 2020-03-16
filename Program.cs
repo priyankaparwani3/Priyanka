01af8c37ff808de427294657b8a89f108f3a2b3d
priyankaparwani3 <me.priyankaparwani@gmail.com>
2020-03-16 17:48:15 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,21 +1,13 @@
 ﻿using System;
 
-namespace Project1
+namespace Project2
 {
     class Program
     {
         static void Main()
         {
-                Console.WriteLine("Enter your First name");
-
-                string FirstName = Console.ReadLine();
-                Console.WriteLine("Enter your Last name");
-
-                string LastName = Console.ReadLine();
-
-                Console.WriteLine("Hello {0} {1}", FirstName, LastName);
-
-                //Console.WriteLine("Hello " + FirstName,LastName);
+            int i = 0;
+            Console.WriteLine("Minimum Value {0}", int.MinValue);
         }
     }
 }
