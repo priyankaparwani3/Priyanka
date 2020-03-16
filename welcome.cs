853932bf412dfb84bcc8f91c66d7b353cde1afe9
priyankaparwani3 <me.priyankaparwani@gmail.com>
2020-03-16 20:45:53 +0530

    Add files via upload

---
+++ welcome.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main1()
+    {
+        Console.WriteLine("Welcome to c# training1!");
+    }
+    static void Main()
+    {
+        Console.WriteLine("Welcome to c# training!");
+        Main1();
+    }
+}
