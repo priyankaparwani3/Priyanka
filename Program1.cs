616370d8af6964d44bc69d13d8add42b02ac2fff
priyankaparwani3 <me.priyankaparwani@gmail.com>
2020-03-16 17:50:04 +0530

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main()
+        {
+                Console.WriteLine("Enter your First name");
+
+                string FirstName = Console.ReadLine();
+                Console.WriteLine("Enter your Last name");
+
+                string LastName = Console.ReadLine();
+
+                Console.WriteLine("Hello {0} {1}", FirstName, LastName);
+
+                //Console.WriteLine("Hello " + FirstName,LastName);
+        }
+    }
+}
