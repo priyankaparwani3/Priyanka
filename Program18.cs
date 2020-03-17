f748ee38efa7759e0a4c8bb0fc00989ead30e293
priyankaparwani3 <me.priyankaparwani@gmail.com>
2020-03-17 15:52:11 +0530

    Add files via upload

---
+++ Program18.cs
@@ -0,0 +1,60 @@
+﻿
+
+
+using System;
+
+
+
+namespace Tutlane
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Users user = new Users("Suresh Dasari", 30);
+
+            user.GetUserDetails();
+
+            Console.WriteLine("Press Enter Key to Exit..");
+
+            Console.ReadLine();
+
+        }
+
+    }
+
+    public class Users
+
+    {
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public Users(string name, int age)
+
+        {
+
+            Name = name;
+
+            Age = age;
+
+        }
+
+        public void GetUserDetails()
+
+        {
+
+            Console.WriteLine("Name: {0}, Age: {1}", Name, Age);
+
+        }
+
+    }
+
+}
