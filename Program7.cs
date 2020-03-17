4a56c34a20eb88095dc786a3392d6169d8988fa5
priyankaparwani3 <me.priyankaparwani@gmail.com>
2020-03-17 12:50:17 +0530

    Add files via upload

---
+++ Program7.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Program
+{
+    public static void Main()
+    {
+        // Initialize and assign values in different lines
+        int[] EvenNumbers = new int[3];
+        EvenNumbers[0] = 0;
+        EvenNumbers[1] = 2;
+        EvenNumbers[2] = 4;
+
+        // Initialize and assign values in the same line
+        int[] OddNumbers = { 1, 3, 5 };
+
+        Console.WriteLine("Printing EVEN Numbers");
+
+        // Retrieve and print even numbers from the array
+        for (int i = 0; i < EvenNumbers.Length; i++)
+        {
+            Console.WriteLine(EvenNumbers[i]);
+        }
+
+        Console.WriteLine("Printing ODD Numbers");
+
+        // Retrieve and print odd numbers from the array
+        for (int i = 0; i < OddNumbers.Length; i++)
+        {
+            Console.WriteLine(OddNumbers[i]);
+        }
+    }
+}
+
