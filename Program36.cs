f8cda743179c78a88d4ab4eb16658154881cdc62
priyankaparwani3 <me.priyankaparwani@gmail.com>
2020-03-17 18:58:28 +0530

    Add files via upload

---
+++ Program36.cs
@@ -0,0 +1,69 @@
+﻿
+
+using System;
+
+
+
+namespace Tutlane
+
+{
+
+    // Declare Delegate
+
+    public delegate void SampleDelegate(int a, int b);
+
+    class MathOperations
+
+    {
+
+        public void Add(int a, int b)
+
+        {
+
+            Console.WriteLine("Add Result: {0}", a + b);
+
+        }
+
+        public void Subtract(int x, int y)
+
+        {
+
+            Console.WriteLine("Subtract Result: {0}", x - y);
+
+        }
+
+    }
+
+    class Program
+
+    {
+
+
+
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("****Delegate Example****");
+
+            MathOperations m = new MathOperations();
+
+            // Instantiate delegate with add method
+
+            SampleDelegate dlgt = m.Add;
+
+            dlgt(10, 90);
+
+            // Instantiate delegate with subtract method
+
+            dlgt = m.Subtract;
+
+            dlgt(10, 90);
+
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
