80dbad11dcfc350db0454bfa1f21aa561baec66e
priyankaparwani3 <me.priyankaparwani@gmail.com>
2020-03-17 12:36:09 +0530

    Add files via upload

---
+++ Program5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+    class Program
+    {
+    static void Main1()
+    {
+        int AvailableTickets;
+        int? TicketsOnSale = null;
+
+        if (TicketsOnSale == null)
+        {
+            AvailableTickets = 0;
+        }
+        else
+        {
+            AvailableTickets = (int)TicketsOnSale;
+        }
+
+        Console.WriteLine("Available Tickets={0}", AvailableTickets);
+    }
+
+    static void Main()
+    { 
+        int AvailableTickets;
+        int? TicketsOnSale = null;
+
+        //Using null coalesce operator ??
+        AvailableTickets = TicketsOnSale ?? 0;
+
+        Console.WriteLine("Available Tickets={0}", AvailableTickets);
+        Main1();
+    }
+    }
