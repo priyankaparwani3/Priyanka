bebef2d14350333b0017edecfd86ef6f26a7e518
priyankaparwani3 <me.priyankaparwani@gmail.com>
2020-03-17 13:37:53 +0530

    Add files via upload

---
+++ Program16&17.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Demo
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int FN = 10;
+            int SN = 20;
+            //FN and LN are method arguments
+            int Total = Sum(FN, SN);
+            Console.WriteLine(Total);
+        }
+        //FirstNumber and SecondNumber are method parameters
+        public static int Sum(int FirstNumber, int SecondNumber)
+        {
+            int Result = FirstNumber + SecondNumber;
+            return Result;
+        }
+    }
+}
