4f95f21a1239bded5662d4cd6491ee96d752c6fa
priyankaparwani3 <me.priyankaparwani@gmail.com>
2020-03-17 12:47:21 +0530

    Add files via upload

---
+++ Program6.cs
@@ -0,0 +1,34 @@
+﻿using System;
+class Program
+{
+    static void Main1()
+    {
+        int i = 100;
+
+        // float is bigger datatype than int. So, no loss of
+        // data and exceptions. Hence implicit conversion
+        float f = i;
+
+        Console.WriteLine(f);
+    }
+    static void Main()
+    {
+        float f = 100.25F;
+
+        // Cannot implicitly convert float to int.
+        // Fractional part will be lost. Float is a
+        // bigger datatype than int, so there is
+        // also a possiblity of overflow exception
+        // int i = f;
+
+        // Use explicit conversion using cast () operator
+        int i = (int)f;
+
+        // OR use Convert class
+        // int i = Convert.ToInt32(f);
+
+        Console.WriteLine(i);
+        Main1();
+    }
+}
+
