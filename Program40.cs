fce3026bee98258201677be5f6e4ef5a05403c8f
priyankaparwani3 <me.priyankaparwani@gmail.com>
2020-03-17 20:44:26 +0530

    Add files via upload

---
+++ Program40.cs
@@ -0,0 +1,58 @@
+﻿
+
+using System;
+
+
+
+namespace Examples
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            string name = null;
+
+            try
+
+            {
+
+                if (name.Length > 0) // Exception will occur
+
+                {
+
+                    Console.WriteLine("Name: " + name);
+
+                }
+
+            }
+
+            catch (Exception ex)
+
+            {
+
+                Console.WriteLine("Exception: {0}", ex.Message);
+
+            }
+
+            finally
+
+            {
+
+                Console.WriteLine("Finally Block.");
+
+            }
+
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
+
