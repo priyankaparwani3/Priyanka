6849c593c038d07130d260fa8321b972d2cb4572
priyankaparwani3 <me.priyankaparwani@gmail.com>
2020-03-17 13:30:17 +0530

    Add files via upload

---
+++ Program10.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Enter a number");
+        int UserNumber = int.Parse(Console.ReadLine());
+
+        switch (UserNumber)
+        {
+            case 10:
+                Console.WriteLine("your number is 10");
+                break;
+            case 20:
+                Console.WriteLine("your number is 20");
+                break;
+            case 30:
+                Console.WriteLine("your number is 30");
+                break;
+            default:
+                Console.WriteLine("your number is not 10, 20 & 30");
+                break;
+
+        }
+        }
+    }
+
