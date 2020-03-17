cb182395bd9d6ef1057f93d54eda07ed310974c1
priyankaparwani3 <me.priyankaparwani@gmail.com>
2020-03-17 18:46:30 +0530

    Add files via upload

---
+++ Program27.cs
@@ -0,0 +1,91 @@
+﻿
+
+
+using System;
+
+
+
+namespace Tutlane
+
+{
+
+    class User
+
+    {
+
+        private string location;
+
+        private string name = "Suresh Dasari";
+
+        public string Location
+
+        {
+
+            get { return location; }
+
+            set { location = value; }
+
+        }
+
+        public string Name
+
+        {
+
+            get
+
+            {
+
+                return name.ToUpper();
+
+            }
+
+            set
+
+            {
+
+                if (value == "Suresh")
+
+                    name = value;
+
+            }
+
+        }
+
+    }
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            User u = new User();
+
+            // set accessor will invoke
+
+            u.Name = "Rohini";
+
+            // set accessor will invoke
+
+            u.Location = "Hyderabad";
+
+            // get accessor will invoke
+
+            Console.WriteLine("Name: " + u.Name);
+
+            // get accessor will invoke
+
+            Console.WriteLine("Location: " + u.Location);
+
+            Console.WriteLine("\nPress Enter Key to Exit..");
+
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
+
