63d3221c5b17d0bc4312965b9561a4eb6c305066
priyankaparwani3 <me.priyankaparwani@gmail.com>
2020-03-17 13:58:33 +0530

    Add files via upload

---
+++ Program15.cs
@@ -0,0 +1,38 @@
+﻿
+
+using System;
+
+
+
+namespace Tutlane
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            string[] names = new string[3] { "Priyanka", "Deepak", "Varsha" };
+
+            foreach (string name in names)
+
+            {
+
+                Console.WriteLine(name);
+
+            }
+
+            Console.WriteLine("Press Enter Key to Exit..");
+
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
+
