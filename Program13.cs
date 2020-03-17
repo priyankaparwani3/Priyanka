aa801a456e3bf3b99fbc70c737cefb0c433e93fc
priyankaparwani3 <me.priyankaparwani@gmail.com>
2020-03-17 13:50:16 +0530

    Add files via upload

---
+++ Program13.cs
@@ -0,0 +1,40 @@
+﻿
+
+using System;
+
+
+
+namespace Tutlane
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            int i = 1;
+
+            while (i <= 4)
+
+            {
+
+                Console.WriteLine("i value: {0}", i);
+
+                i++;
+
+            }
+
+            Console.WriteLine("Press Enter Key to Exit..");
+
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
+
