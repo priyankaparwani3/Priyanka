bd038ac2cf9d4b98fed3d0ce8a4d6455b0801815
priyankaparwani3 <me.priyankaparwani@gmail.com>
2020-03-20 20:19:32 +0530

    Add files via upload

---
+++ Program32.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public abstract class Customer
+{
+    public abstract void Print();
+
+}
+public class Program : Customer
+{
+    public override void Print()
+    {
+        Console.WriteLine("Print method");
+    }
+    public static void Main()
+    {
+        Customer C = new Program();
+        C.Print();
+
+    }
+}
