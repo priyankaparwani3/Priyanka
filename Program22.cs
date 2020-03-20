5c40a218ebb48b3e56d34387d2071855d46ae893
priyankaparwani3 <me.priyankaparwani@gmail.com>
2020-03-20 13:25:57 +0530

    Add files via upload

---
+++ Program22.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class Employee
+{
+    public string FirstName;
+    public string LastName;
+
+    public void PrintFullName()
+    {
+        Console.WriteLine(FirstName + " " + LastName);
+
+    }
+}
+
+public class PartTimeEmployee : Employee
+{
+    public new void PrintFullName()
+    {
+        base.PrintFullName();
+
+    }
+}
+
+public class FullTimeEmployee : Employee
+{
+
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        FullTimeEmployee FTE = new FullTimeEmployee();
+        FTE.FirstName = "FullTime";
+        FTE.LastName = "Employee";
+        FTE.PrintFullName();
+
+        PartTimeEmployee PTE = new PartTimeEmployee();
+        PTE.FirstName = "PartTime";
+        PTE.LastName = "Employee";
+        PTE.PrintFullName();
+    }
+}
+
