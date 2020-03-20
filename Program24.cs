d5929eb0d234db27543a8395ac7914ef1431a9af
priyankaparwani3 <me.priyankaparwani@gmail.com>
2020-03-20 17:45:13 +0530

    Add files via upload

---
+++ Program24.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+    public class BaseClass
+    {
+        public virtual void Print()
+        {
+            Console.WriteLine("i am a baseclass print method");
+        }
+    }
+public class DerivedClass : BaseClass
+{
+    public override void Print()
+    {
+        Console.WriteLine("i am a baseclass print method");
+
+    }
+}
+public class Program
+{
+    public static void Main()
+    {
+        BaseClass B = new DerivedClass();
+        B.Print();
+    }
+
+}
+
+
