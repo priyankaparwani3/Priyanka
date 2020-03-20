691276e40fc95514a5dd33f0b1cf040ba578398a
priyankaparwani3 <me.priyankaparwani@gmail.com>
2020-03-20 12:15:03 +0530

    Add files via upload

---
+++ Program20.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+class Circle
+{
+    float _PI = 3.141F;
+    int _Radius;
+    public Circle(int Radius)
+    {
+        this._Radius = Radius;
+    }
+    public float CalculateArea()
+    {
+        return this._PI * this._Radius * this._Radius;
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Circle C1 = new Circle(5);
+        float Area = C1.CalculateArea();
+        Console.WriteLine("Area = {0}", Area);
+    }
+
+}
