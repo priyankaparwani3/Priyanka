6d094a8f04150ccf8b960fec27a8e81528bf6539
priyankaparwani3 <me.priyankaparwani@gmail.com>
2020-03-20 20:12:07 +0530

    Add files via upload

---
+++ Program29.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Customer
+{
+    public int ID { get; set; }
+    public string Name
+    {
+        get; set;
+    }
+}
+public class Program
+{
+    public static void Main()
+    {
+        int i = 10;
+        int j = i;
+        j = j + 1;
+
+        Console.WriteLine("i = {0} && j = {1}", i, j);
+            Customer C1 = new Customer();
+            C1.ID = 101;
+            C1.Name = "Mark";
+
+        Customer C2 = C1;
+        C2.Name = "Mary";
+        Console.WriteLine("C1.Name = {0} && C2.Name = {1}", C1.Name, C2.Name);
+
+        }
+    }
